9763bc383b18b5768092d8c115d1d87981a13ed4
Tarun4300 <taru0007@e.ntu.edu.sg>
2025-04-05 01:21:32 +0800

    Update Enquiry.java

--- Source Codes/Enquiry.java
+++ Source Codes/Enquiry.java
@@ -1,4 +1,18 @@
 
 public class Enquiry {
+  private int enquiryId;
+	private Project project;
+	
+	public Enquiry(int enquiryId, Project project) {
+		this.enquiryId = enquiryId;
+		this.project = project;
+		
+	}
+	public int getEnquiryId() {
+		return enquiryId;
+	}
+	public Project getProject() {
+		return project;
+	}
 
 }
